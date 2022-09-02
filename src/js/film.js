acd4770a6ffd89f02da5434b83c5ba9d9c93f824
Kl0filinj <vladdanya08@gmail.com>
2022-09-02 21:56:38 +0300

    pause

--- src/js/film.js
+++ src/js/film.js
@@ -1,4 +1,12 @@
 export class Film {
+  static firebaseConfig = {
+    apiKey: 'AIzaSyCv6MxU8A-pjcYfh7e--9UuG9oU8ESw1yo',
+    authDomain: 'test-template-f17b9.firebaseapp.com',
+    projectId: 'test-template-f17b9',
+    storageBucket: 'test-template-f17b9.appspot.com',
+    messagingSenderId: '739422049395',
+    appId: '1:739422049395:web:61f14da0b2bfc36e7f8606',
+  };
   static createWithoutAuth(newFilm) {
     const all = getFilmsFromLocalStorge();
     all.push(newFilm);
@@ -23,6 +31,5 @@ function renderOneFilm(film) {
     <li>
         <p class="h4">Название: ${film.title}</p>
         <p class="h4">Рейтинг: <span class="rating">${film.rating}</span></p>
-    </li>
-    `;
+    </li>`;
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,19 @@
 import { validForm } from './utils';
 import { Film } from './film';
+import * as bootstrap from 'bootstrap';
 
 const refs = {
   filmForm: document.getElementById('film-form'),
   filmName: document.getElementById('film'),
   filmRating: document.getElementById('rating'),
   filmSubmitBtn: document.getElementById('film-submit-btn'),
+  authModalBtn: document.getElementById('my-modal-btn'),
+  authModal: document.getElementById('my-modal'),
 };
 
+refs.authModalBtn.addEventListener('click', () => {
+  refs.authModal.show;
+});
 window.addEventListener('load', Film.renderFilmList);
 refs.filmForm.addEventListener('submit', filmSubmitHandler);
 refs.filmForm.addEventListener('input', () => {
